efc39369f18b8f121191a338575e53f0d467f83e
Aden Herold <aden.herold1@gmail.com>
2019-08-30 13:07:11 +0930

    Configured webpack + dev-server and babel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 
-const Hi = () => <p>Hi</p>;
+const Hi = () => {
+  return <p>Hi</p>;
+};
 
 render(<Hi />, document.getElementById("app"));
