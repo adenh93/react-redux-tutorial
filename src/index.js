042d73da05c64c973f0fe7af480ce086249b0425
Aden Herold <aden.herold1@gmail.com>
2019-08-31 13:00:27 +0930

    Implemented basic boilerplate redux code. Created first action and reducer for courses

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
+import configureStore from "./redux/configureStore";
+import { Provider } from "react-redux";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
+const store = configureStore();
+
 render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("app")
 );
