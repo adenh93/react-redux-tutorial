24915721b2f6073d5f0691013fa0f8f592ff894b
Aden Herold <aden.herold1@gmail.com>
2019-08-31 14:53:55 +0930

    Added CourseList component from the PluralSight course exercise files and imported into the CoursesPage component. Courses are now displayed in a table with details

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
-import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
+import CourseList from "./CourseList";
 
 class CoursesPage extends React.Component {
   componentDidMount() {
@@ -15,9 +16,7 @@ class CoursesPage extends React.Component {
     return (
       <div className="courses">
         <h1>Courses</h1>
-        {this.props.courses.map(course => (
-          <div key={course.title}>{course.title}</div>
-        ))}
+        <CourseList courses={this.props.courses} />
       </div>
     );
   }
